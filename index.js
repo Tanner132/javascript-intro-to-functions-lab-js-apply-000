90b42db985ec28ae9734541a1a9e5990830c73be
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-11 02:19:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function LogShout(string) {
+  return console.log(string).toUpperCase
+}
