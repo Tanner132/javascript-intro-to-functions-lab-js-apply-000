2ab23db63fca7b0984fbf1603a3d17a8964667be
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-11 02:38:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma.")
   {
     return "I love you, too.";
-  } else if (string === string.toLowerCase());
+  } if else (string === string.toLowerCase());
   {
     return "I can't hear you!";
   } else
